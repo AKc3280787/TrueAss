e36e900448354470bbbb556b62b4a084475c1e9f
Adam Keating <a.j.keating@outlook.com>
2019-04-25 09:35:38 +0000

    Finished the task list

--- core.c
+++ core.c
@@ -3,4 +3,9 @@
     1. Encypt a message with a function using a key from a file
        which will also be written to a file.
     2. Do the reverse of point 1.
-    3. */
+    3. Encypt with a substaution chipher with a message and key
+    4. do the reverse of 3.
+    5. Decypt a message that is encypted with rotation cypher
+       without the key. AKA Break the code.
+    6. Do the same with 5 but with a sub encyption.
+    */
