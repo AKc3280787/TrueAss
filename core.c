a5e1bcc5190010ef5af641b1e5408523a4f42bf9
Adam Keating <a.j.keating@outlook.com>
2019-04-25 09:33:15 +0000

    Writting up task

--- core.c
+++ core.c
@@ -1,2 +1,6 @@
-/*  Testing if the thing is working or not */
-int yadayaydsa
+/*  Current goals of this program as specified by the document
+    is as follows:
+    1. Encypt a message with a function using a key from a file
+       which will also be written to a file.
+    2. Do the reverse of point 1.
+    3. */
